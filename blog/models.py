acdb2f48ea28334e28e16f8ca92d407170db8493
ValentinaLV <tina.lysenok@gmail.com>
2020-01-17 13:47:45 +0200

    issue #15

--- blog/models.py
+++ blog/models.py
@@ -51,3 +51,13 @@ class Tag(models.Model):
 
     def __str__(self):
         return f"{self.title}"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.post}"
