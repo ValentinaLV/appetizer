b952f18c98bc038c10e2916d994cc71784cae5dd
ValentinaLV <tina.lysenok@gmail.com>
2020-01-21 21:59:03 +0200

    issue #31

--- order/forms.py
+++ order/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+
 from .models import Order
 
 
@@ -8,3 +9,13 @@ class OrderCreateForm(forms.ModelForm):
         fields = ['billing_first_name', 'billing_last_name',
                   'phone', 'email', 'billing_address', 'billing_postal_code',
                   'billing_city']
+
+        widgets = {
+            'billing_first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}),
+            'billing_last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '+38093 777 7777', 'required': 'required'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'appetizer@email.com'}),
+            'billing_address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address'}),
+            'billing_postal_code': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '01034'}),
+            'billing_city': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'City'}),
+        }
